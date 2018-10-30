d79deb9c4b494c5fe6dda163af13d2b31f06b878
heshij <1634943446@qq.com>
2018-10-30 19:03:41 +0800

    init

--- src/pages/Home/components/NewPro.vue
+++ src/pages/Home/components/NewPro.vue
@@ -2,59 +2,91 @@
     <div class="newPro">
         <ul>
             <li>
-                <h2></h2>
-                <span></span>
+                <div class="text">
+                    <h2>全场满350减100</h2>
+                    <span>全场低至1-3折，满就送</span>
+                </div>
                 <router-link to="/">
-                    <img src="" alt="">
+                   <div class="image">
+                       <img src="../../.././images/home/newPro01.jpg" alt="">
+                   </div>
                 </router-link>
             </li>
             <li>
-                <h2></h2>
-                <span></span>
+                <div class="text">
+                    <h2>B系列</h2>
+                    <span>1-3折起</span>
+                </div>
                 <router-link to="/">
-                    <img src="" alt="">
+                    <div class="image">
+                        <img src="../../.././images/home/newPro02.jpg" alt="">
+                    </div>
                 </router-link>
             </li>
             <li>
-                <h2></h2>
-                <span></span>
+                <div class="text">
+                    <h2>B系列</h2>
+                    <span>1-3折起</span>
+                </div>
                 <router-link to="/">
-                    <img src="" alt="">
+                    <div class="image">
+                        <img src="../../.././images/home/newPro03.jpg" alt="">
+                    </div>
                 </router-link>
             </li>
             <li>
-                <h2></h2>
-                <span></span>
+                <div class="text">
+                    <h2>B系列</h2>
+                    <span>1-3折起</span>
+                </div>
                 <router-link to="/">
-                    <img src="" alt="">
+                    <div class="image">
+                        <img src="../../.././images/home/newPro04.jpg" alt="">
+                    </div>
                 </router-link>
             </li>
             <li>
-                <h2></h2>
-                <span></span>
+                <div class="text">
+                    <h2>B系列</h2>
+                    <span>1-3折起</span>
+                </div>
                 <router-link to="/">
-                    <img src="" alt="">
+                    <div class="image">
+                        <img src="../../.././images/home/newPro05.jpg" alt="">
+                    </div>
                 </router-link>
             </li>
             <li>
-                <h2></h2>
-                <span></span>
+                <div class="text">
+                    <h2>B系列</h2>
+                    <span>1-3折起</span>
+                </div>
                 <router-link to="/">
-                    <img src="" alt="">
+                    <div class="image">
+                        <img src="../../.././images/home/newPro06.jpg" alt="">
+                    </div>
                 </router-link>
             </li>
             <li>
-                <h2></h2>
-                <span></span>
+                <div class="text">
+                    <h2>B系列</h2>
+                    <span>1-3折起</span>
+                </div>
                 <router-link to="/">
-                    <img src="" alt="">
+                    <div class="image">
+                        <img src="../../.././images/home/newPro07.jpg" alt="">
+                    </div>
                 </router-link>
             </li>
             <li>
-                <h2></h2>
-                <span></span>
+                <div class="text">
+                    <h2>B系列</h2>
+                    <span>1-3折起</span>
+                </div>
                 <router-link to="/">
-                    <img src="" alt="">
+                    <div class="image">
+                        <img src="../../.././images/home/newPro08.jpg" alt="">
+                    </div>
                 </router-link>
             </li>
         </ul>
@@ -86,6 +118,32 @@
         height: 162px;
         margin-bottom: 10px;
         background-color: skyblue;
+        position: relative;
+    }
+    .newPro ul li .text{
+        width: inherit;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        z-index: 99;
+        padding-top: 18px;
+    }
+    .newPro ul li .image{
+        width: 162px;
+        height: 162px;
+        position: relative;
+
+    }
+    .newPro ul li .image img{
+        position: absolute;
+        left: 0;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        margin: auto;
+        width: 100%;
     }
     .newPro ul li:nth-child(1){
         width: 334px;
@@ -96,5 +154,12 @@
         position: absolute;
         top: 172px;
         right: 0;
+    }
+    .newPro ul li:nth-child(1) .image{
+        width: 334px;
+        height: 334px;
+    }
+    .newPro ul li:nth-child(4) .image{
+        width: 334px;
     }
 </style>
