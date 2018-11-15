9e9d03107b31e94022d17e8324ee48ea20b98607
heshij <1634943446@qq.com>
2018-11-15 20:55:09 +0800

    init

--- src/pages/ShopCar/ShopCar.vue
+++ src/pages/ShopCar/ShopCar.vue
@@ -1,18 +1,44 @@
 <template>
     <div class="ShopCar">
         <shop-header></shop-header>
+        <div class="main">
+            <full-shop-car v-if="bool"></full-shop-car>
+            <null-shop-car v-else></null-shop-car>
+        </div>
         <MenuBar></MenuBar>
     </div>
 </template>
 
 <script>
     import ShopHeader from "./components/shopHeader";
+    import NullShopCar from "./components/nullShopCar";
+    import FullShopCar from "./components/fullShopCar";
     export default {
         name: "ShopCar",
-        components: {ShopHeader}
+        components: {FullShopCar, NullShopCar, ShopHeader},
+        data(){
+            return{
+                bool:false
+            }
+        },
+        mounted(){
+            if(localStorage.getItem("proItems")){
+                this.bool = true
+                console.log(localStorage.getItem("proItems"))
+            }else{
+                this.bool = false
+            }
+        },
+        methods:{
+            proItemsNo(){
+                this.bool = false
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    .ShopCar{
+        padding-top: 96px;
+    }
 </style>
---
+++ src/pages/ShopCar/components/fullShopCar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="fullShopCar">
+        <div class="bigProList">
+            <div class="Merchant">
+                <form action="">
+                    <input type="checkbox" class="merAllCheck">
+                    <router-link to="/fullShopCar" class="iconfont">
+                        &#xe619;
+                        <span>kelly家的故事</span>
+                        <i>&#xe619;</i>
+                    </router-link>
+                    <span>领券</span>
+                </form>
+                <span>编辑</span>
+            </div>
+            <div class="proList">
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "fullShopCar"
+    }
+</script>
+
+<style scoped>
+
+</style>
